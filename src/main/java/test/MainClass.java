ca935f6e13b21fdf28b7c8dbf7f9751984a06275
Ghayeth Arbi <ghayeth.arbi@esprit.tn>
2024-04-14 22:17:10 +0200

    v3 db

--- src/main/java/test/MainClass.java
+++ src/main/java/test/MainClass.java
@@ -1,6 +1,7 @@
 package test;
 
 import models.User;
+import services.ServiceCategory;
 import services.ServiceUser;
 import utils.DBConnection;
 
@@ -12,12 +13,14 @@ public class MainClass {
         DBConnection cn1 = DBConnection.getInstance();
 
         User u = new User("Ben Daoued","Yosra", "yosra@gmail.com");
-        
+        ServiceCategory sc = new ServiceCategory();
         ServiceUser sp = new ServiceUser();
 
         try {
             //sp.insertOne(u);
             System.out.println(sp.selectAll());
+            System.out.println(sc.selectAll());
+
         } catch (SQLException e) {
             System.err.println("Erreur: "+e.getMessage());
         }
--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/gestiuser";
+    private static final String URL = "jdbc:mysql://localhost:3306/fitkeep";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
